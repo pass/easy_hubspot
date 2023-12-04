1c4c756ace5eb1cb65cfbf7e894201b1ad1215f7
Kyle Keesling <me@kylekeesling.com>
2023-12-04 14:43:20 -0500

    Update list.rb

--- lib/easy_hubspot/list.rb
+++ lib/easy_hubspot/list.rb
@@ -1,36 +1,26 @@
 # frozen_string_literal: true
 
 module EasyHubspot
-  # class EasyHubspot::Contact
   class List < EasyHubspot::Base
     class << self
       LIST_ENDPOINT = 'crm/v3/lists'
 
-      # def get_list(list_id)
-      #   Client.do_get(list_id_endpoint(list_id), headers)
-      # end
-
-      def get_lists
-        Client.do_get(LIST_ENDPOINT, headers)
-      end
-
-      # def create_list(body)
-      #   Client.do_post(LIST_ENDPOINT, body, headers)
-      # end
-
-      # def update_list(list_id, body)
-      #   Client.do_patch(list_id_endpoint(list_id), body, headers)
-      # end
-
-      # def delete_list(list_id)
-      #   Client.do_delete(list_id_endpoint(list_id), headers)
-      # end
-
-      def create_list_membership(list_id, body)
+      # Add Records to List call: https://developers.hubspot.com/docs/api/crm/lists
+      # contact_ids must be either an array or a string/integer of a Contact ID
+      def create_list_membership(list_id, contact_ids)
         membership_endpoint = "#{list_id_endpoint(list_id)}/memberships/add"
+
+        # for some reason this endpoint doesnt accept a JSON object, just
+        # a stringified array of Contact IDs
+        body =
+          if contact_ids.is_a?(Array)
+            contact_ids.to_s
+          else
+            "[#{contact_ids}]"
+          end
+
         Client.do_put(membership_endpoint, body, headers)
       end
-
 
       private
 
